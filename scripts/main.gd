8a84643cf50b9a651ebebd005ba02b46d514261b
KevFR <ikevfr@gmail.com>
2024-03-24 21:55:19 +0100

    Uploading main files

---
+++ scripts/main.gd
@@ -0,0 +1,82 @@
+extends Area2D
+
+var tiles = []
+var solved = []
+var mouse = false
+
+func _ready():
+	start_game()
+
+func start_game():
+	tiles = [$Tile1, $Tile2, $Tile3, $Tile4, $Tile5, $Tile6, $Tile7, $Tile8, $Tile9, $Tile10, $Tile11, $Tile12, $Tile13, $Tile14, $Tile15, $Tile16 ]
+	solved = tiles.duplicate()
+	shuffle_tiles()
+	
+func shuffle_tiles():
+	var previous = 73
+	var previous_1 = 72
+	for t in range(0,311):
+		var tile = randi() % 8
+		if tiles[tile] != $Tile16 and tile != previous and tile != previous_1:
+			var rows = int(tiles[tile].position.y / 73)
+			var cols = int(tiles[tile].position.x / 73)
+			check_neighbours(rows,cols)
+			previous_1 = previous
+			previous = tile
+			
+func _process(_delta):
+	if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT) and mouse:
+		var mouse_copy = mouse
+		mouse = false
+		var rows = int(mouse_copy.position.y / 73)
+		var cols = int(mouse_copy.position.x / 73)
+		check_neighbours(rows,cols)
+		if tiles == solved:
+			print("You win!")
+
+func check_neighbours(rows, cols):
+	var empty = false
+	var done = false
+	var pos = rows * 4 + cols
+	while !empty and !done:
+		var new_pos = tiles[pos].position
+		if rows < 4:
+			new_pos.y += 73
+			empty = find_empty(new_pos,pos)
+			new_pos.y -= 73
+		if rows > 0:
+			new_pos.y -= 73
+			empty = find_empty(new_pos,pos)
+			new_pos.y += 73
+		if cols < 4:
+			new_pos.x += 73
+			empty = find_empty(new_pos,pos)
+			new_pos.x -= 73
+		if cols > 0:
+			new_pos.x -= 73
+			empty = find_empty(new_pos,pos)
+			new_pos.x += 73
+		done = true
+			
+func find_empty(position,pos):
+	var new_rows = int(position.y / 73)
+	var new_cols = int(position.x / 73)
+	var new_pos = new_rows * 3 + new_cols
+	if tiles[new_pos] == $Tile16:
+		swap_tiles(new_pos, pos)
+		return true
+	else:
+		return false
+		
+func swap_tiles(tile_src, tile_dst):
+	var temp_pos = tiles[tile_src].position
+	tiles[tile_src].position = tiles[tile_dst].position
+	tiles[tile_dst].position = temp_pos
+	var temp_tile = tiles[tile_src]
+	tiles[tile_src] = tiles[tile_dst]
+	tiles[tile_dst] = temp_tile
+	
+func _input_event(viewport, event, shape_idx):
+	if event is InputEventMouseButton:
+		mouse = event
+	
